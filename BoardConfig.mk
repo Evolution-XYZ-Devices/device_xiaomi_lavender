466fb6bc5e9dc89f8ac14d7b4600840b0a2060d1
rena2806 <renascape@gmail.com>
2020-04-21 09:46:03 +0200

    lavender: Update DT2W node
    Change-Id: I81c0ceb5744b505775ed12841dbc78e13fea9067

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,7 +39,7 @@ DEVICE_PATH := device/xiaomi/lavender
 TARGET_SCREEN_DENSITY := 403
 
 # DT2W
-TARGET_TAP_TO_WAKE_NODE := "/proc/tp_gesture"
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
 # Enable System As Root even for non-A/B from P onwards
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
