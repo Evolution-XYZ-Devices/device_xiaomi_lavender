84881f82868c75f7b3af2bbda824ef2a9a92bdc6
pix106 <sbordenave@gmail.com>
2021-11-22 23:41:22 +0100

    lavender: clean duplicated product inherits
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- evolution_lavender.mk
+++ evolution_lavender.mk
@@ -21,10 +21,6 @@
 # definition file).
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/lavender/device.mk)
 
