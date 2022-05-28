a60deeb98c46cc9c8a323cf9561f86af357a53da
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-28 10:27:37 +0200

    lavender: Drop atrace HIDL
    Change-Id: Iaaaf62e9dda26dd15d51f5975d9f21fe1037d2eb
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>

--- device.mk
+++ device.mk
@@ -71,10 +71,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     AntHalService
 
-# ATRACE_HAL
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl:32 \
