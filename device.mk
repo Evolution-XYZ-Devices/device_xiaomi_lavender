b2a68453a7cd01076207e9d25926cc902c48fcd1
Carter Hsu <carterhsu@google.com>
2022-11-08 20:53:42 +0530

    lavender: Update Audio HIDL to 7.1
    Bug: 205235270
    Test: build pass
    Signed-off-by: Carter Hsu <carterhsu@google.com>
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>
    Change-Id: Ic54e2787ef27022556430c6c1db346b1e1348e39

--- device.mk
+++ device.mk
@@ -73,7 +73,7 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@7.0-impl:32 \
+    android.hardware.audio@7.1-impl:32 \
     android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.0-impl:32 \
