dc29817c20bcd431d33891bd600c25bca234702a
Kyle Harrison <khwebmail@gmail.com>
2022-12-11 21:32:22 +0530

    lavender: Don't write binary XML files
    - Android 12 now writes XML files in binary format by default. This can cause incompatibility with TWRP which can hang when attempting to read XML files e.g. /data/system/storage.xml
    
    - This commit sets the persist.sys.binary_xml property to false so that XML files are written in text format.
    
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>
    Change-Id: If6430597d8459e62dd78aa458c58237c71618970

--- device.mk
+++ device.mk
@@ -374,6 +374,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcamera_sdm660_shim
 
+# System
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.binary_xml=false
+
 # Tetheroffload
 PRODUCT_PACKAGES += \
     ipacm \
