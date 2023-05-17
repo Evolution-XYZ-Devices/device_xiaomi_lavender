dbff6e4a9c1ce9502f5a3dbd0d9df698e70b53be
HeroBuxx <me@herobuxx.me>
2023-05-17 05:23:37 +0000

    lavender: parts: Import new Xiaomi Parts from chime
    Imported from: https://github.com/PixelExperience-Devices/device_xiaomi_chime/commit/41b4cbfda5df0e1192d35c703267df04bc14914a
    
    Signed-off-by: HeroBuxx <me@herobuxx.me>
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

--- device.mk
+++ device.mk
@@ -291,6 +291,10 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Parts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr \
