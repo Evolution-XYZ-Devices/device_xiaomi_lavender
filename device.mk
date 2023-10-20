2b55a8c4b94130a57d19ad8a19d2042526b4a50e
Omar Mohamed <omarcoptan9@gmail.com>
2023-10-20 15:27:09 +0000

    Revert "Revert "lavender: Decommonize USB hal""
    This reverts commit 436bf0b2260414e6d870e89d475bf710959c716a.

--- device.mk
+++ device.mk
@@ -54,6 +54,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Wifi
 PRODUCT_PACKAGES += \
     LavenderWifiOverlay
