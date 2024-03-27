b41ddaa18ef7d5b463f861ab411b675166149c01
mikeNG <mikeioannina@gmail.com>
2024-03-27 07:08:31 +0200

    lavender: Switch to android.hardware.usb@1.3-service.dual_role_usb
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

--- device.mk
+++ device.mk
@@ -55,7 +55,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.3-service.dual_role_usb
 
 # VNDK
 PRODUCT_COPY_FILES += \
